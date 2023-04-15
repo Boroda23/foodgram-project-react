4be4c5940dcdf5593de98b30b98995cf5b3be461
Nikolaev Anton <86743560+Boroda23@users.noreply.github.com>
2023-04-15 12:29:54 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,9 +15,6 @@ DEBUG = os.getenv('DEBUG', default='True') == 'True'
 ALLOWED_HOSTS = os.environ.get(
     'ALLOWED_HOSTS', default='*').split(', ')
 
-
-ALLOWED_HOSTS = [*]
-
 AUTH_USER_MODEL = 'users.User'
 
 INSTALLED_APPS = [
